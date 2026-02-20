5f4e8dc2c2cffa5aa381d5c503bd0ee00f63c761
JNaveiro <juanmalorenzonaveiro@gmail.com>
2026-02-20 10:14:35 -0600

    Updated CSS and JavaScript for the Lecture Notes

---
+++ LinearAlgebra/jax.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+    tex: {
+        inlineMath: {'[+]': [['$', '$']]},
+        macros: {
+            RR: "{\\mathbb{R}}",
+            CC: "{\\mathbb{C}}",
+            bf: ["{\\mathbf{#1}}", 1]
+        }
+    }
+};
